941133850fa0ec12226b57029a3d1ccd5deb47d3
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2021-07-27 23:19:38 -0700

    Update to Forge 37.0.13

--- src/main/kotlin/thedarkcolour/kotlinforforge/KotlinForForge.kt
+++ src/main/kotlin/thedarkcolour/kotlinforforge/KotlinForForge.kt
@@ -1,14 +1,7 @@
 package thedarkcolour.kotlinforforge
 
-import net.minecraft.world.item.BlockItem
-import net.minecraft.world.item.Item
-import net.minecraft.world.level.block.Block
-import net.minecraft.world.level.block.state.BlockBehaviour
-import net.minecraft.world.level.material.Material
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
-import net.minecraftforge.registries.ForgeRegistries
-import thedarkcolour.kotlinforforge.forge.KDeferredRegister
 import thedarkcolour.kotlinforforge.forge.MOD_CONTEXT
 
 /**
@@ -22,14 +15,4 @@ import thedarkcolour.kotlinforforge.forge.MOD_CONTEXT
  * check out the [KotlinModdingSkeleton repository](https://github.com/thedarkcolour/KotlinModdingSkeleton).
  */
 @Mod("kotlinforforge")
-public class KotlinForForge {
-    init {
-        val blocks = KDeferredRegister(ForgeRegistries.BLOCKS, "kotlinforforge")
-        val items = KDeferredRegister(ForgeRegistries.ITEMS, "kotlinforforge")
-        blocks.register(FMLJavaModLoadingContext.get().modEventBus)
-        items.register(FMLJavaModLoadingContext.get().modEventBus)
-
-        val red by blocks.register("red_block") { Block(BlockBehaviour.Properties.of(Material.BAMBOO)) }
-        val item = items.register("red_item") { BlockItem(red, Item.Properties()) }
-    }
-}
+public class KotlinForForge
