c4daa944b5016581c6fee2bcec403adb6e53acf2
tmvkrpxl0 <ironman466@naver.com>
2023-01-10 05:17:05 +0900

    Removed duplicates
    Signed-off-by: tmvkrpxl0 <ironman466@naver.com>

--- kfflib/src/main/kotlin/thedarkcolour/kotlinforforge/forge/vectorutil/Vector3fUtil.kt
+++ kfflib/src/main/kotlin/thedarkcolour/kotlinforforge/forge/vectorutil/Vector3fUtil.kt
@@ -3,7 +3,6 @@ package thedarkcolour.kotlinforforge.forge.vectorutil
 import com.mojang.math.Vector3d
 import com.mojang.math.Vector3f
 import net.minecraft.core.Vec3i
-import net.minecraft.world.phys.Vec3
 
 public operator fun Vector3f.plusAssign(other: Vector3f): Unit = add(other)
 
@@ -33,10 +32,6 @@ public operator fun Vector3f.times(times: Float): Vector3f {
 
 public fun Vector3f.clone(): Vector3f {
     return Vector3f(x(), y(), z())
-}
-
-public fun Vector3f(other: Vec3): Vector3f {
-    return Vector3f(other.x.toFloat(), other.y.toFloat(), other.z.toFloat())
 }
 
 public fun Vector3f(other: Vector3d): Vector3f {
