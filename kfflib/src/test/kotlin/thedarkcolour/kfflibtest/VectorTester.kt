7f9fb04f9d3aae91e58a21f8bbd8816585bde15f
tmvkrpxl0 <ironman466@naver.com>
2023-01-13 23:56:37 +0900

    Be quite
    Signed-off-by: tmvkrpxl0 <ironman466@naver.com>

--- kfflib/src/test/kotlin/thedarkcolour/kfflibtest/VectorTester.kt
+++ kfflib/src/test/kotlin/thedarkcolour/kfflibtest/VectorTester.kt
@@ -37,8 +37,6 @@ internal fun testVec2() {
 
     requireEquality(v1, Vec2(x1, y1), Vec2::equals) { "v1 has been mutated!" }
     requireEquality(v2, Vec2(x2, y2), Vec2::equals) { "v2 has been mutated!" }
-
-    KFFLibTest.LOGGER.info("Vec2 test succeed")
 }
 
 internal fun testVec3i() {
@@ -71,8 +69,6 @@ internal fun testVec3i() {
     requireEquality(v1, vec3.toVec3i()) { "Vec3 -> Vec3i conversion has failed!" }
     requireEquality(v1, vector3f.toVec3i()) { "Vector3f -> Vec3i conversion has failed!" }
     requireEquality(v1, vector3d.toVec3i()) { "Vector3d -> Vec3i conversion has failed!" }
-
-    KFFLibTest.LOGGER.info("Vec3i test succeed")
 }
 
 internal fun testVec3() {
@@ -105,8 +101,6 @@ internal fun testVec3() {
     requireEquality(v1, vec3i.toVec3()) { "Vec3i -> Vec3 conversion has failed!" }
     requireEquality(v1, vector3f.toVec3()) { "Vector3f -> Vec3 conversion has failed!" }
     requireEquality(v1, vector3d.toVec3()) { "Vector3d -> Vec3 conversion has failed!" }
-
-    KFFLibTest.LOGGER.info("Vec3 test succeed")
 }
 
 internal fun testVector3d() {
@@ -157,8 +151,6 @@ internal fun testVector3d() {
     requireEquality(v1, vec3i.toVector3d(), tester) { "Vec3i -> Vector3d conversion has failed!" }
     requireEquality(v1, vector3f.toVector3d(), tester) { "Vector3f -> Vector3d conversion has failed!" }
     requireEquality(v1, vec3.toVector3d(), tester) { "Vec3 -> Vector3d conversion has failed!" }
-
-    KFFLibTest.LOGGER.info("Vector3d test succeed")
 }
 
 internal fun testVector3f() {
@@ -203,8 +195,6 @@ internal fun testVector3f() {
     requireEquality(v1, vec3i.toVector3f()) { "Vec3i -> Vector3f conversion has failed!" }
     requireEquality(v1, vector3d.toVector3f()) { "Vector3f -> Vector3f conversion has failed!" }
     requireEquality(v1, vec3.toVector3f()) { "Vec3 -> Vector3f conversion has failed!" }
-
-    KFFLibTest.LOGGER.info("Vector3f test succeed")
 }
 
 internal fun testVector4f() {
@@ -242,8 +232,6 @@ internal fun testVector4f() {
     val timesAssign = v1.clone()
     timesAssign *= scalar
     requireEquality(v1 * scalar, timesAssign) { "Vector4f multiplication&assign has failed!" }
-
-    KFFLibTest.LOGGER.info("Vector4f test succeed")
 }
 
 private fun nextInt() = Random.nextInt(-5000..5000)
