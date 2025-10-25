0d2d002e8f108c9f0c9f515d380e4c7e38fbb9d8
NoodleInWater <why097729@gmail.com>
2025-10-25 13:58:15 +0800

    Update use functions
    `toRun` can be a Consumer so that user can easily call self in the body.

--- src/kfflib/common/kotlin/thedarkcolour/kotlinforforge/forge/ProfilerUtil.kt
+++ src/kfflib/common/kotlin/thedarkcolour/kotlinforforge/forge/ProfilerUtil.kt
@@ -2,14 +2,14 @@ package thedarkcolour.kotlinforforge.forge
 
 import net.minecraft.util.profiling.ProfilerFiller
 
-public fun ProfilerFiller.use(name: String, toProfile: () -> Unit) {
+public fun ProfilerFiller.use(name: String, toProfile: (ProfilerFiller) -> Unit) {
     push(name)
-    toProfile()
+    toProfile(this)
     pop()
 }
 
-public fun ProfilerFiller.use(supplier: () -> String, toProfile: () -> Unit) {
+public fun ProfilerFiller.use(supplier: () -> String, toProfile: (ProfilerFiller) -> Unit) {
     push(supplier)
-    toProfile()
+    toProfile(this)
     pop()
 }
