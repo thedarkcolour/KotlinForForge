201bc49ac339a83331d0cf56c52ee4a1ae012982
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2026-03-09 17:37:57 -0700

    [Release][Maven] Kotlin for Forge 6.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -350,7 +350,7 @@ data class FakeMavenDependency(private val groupId: String, private val artifact
 //
 // MOD PLATFORMS
 //
-val supportedMcVersions = listOf("1.20.6", "1.21", "1.21.1", "1.21.2", "1.21.3", "1.21.4", "1.21.5")
+val supportedMcVersions = listOf("1.21.9", "1.21.10", "1.21.11")
 
 publishMods {
     file = tasks.jar.get().archiveFile
